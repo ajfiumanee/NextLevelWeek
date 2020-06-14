40d1955f79414065f73d4c661b1771a02e740c1c
ajfiumanee <ajfiumanee@outlook.com>
2020-06-14 13:39:32 -0300

    Creating table ITEMS and POINT_ITEMS

--- backend/src/database/migrations/01_create_items.ts
+++ backend/src/database/migrations/01_create_items.ts
@@ -1,9 +1,19 @@
+import Knex from 'knex';
+
 /**
  * Create the table
  */
-export async function up() { }
+export async function up(knex: Knex) {
+    return knex.schema.createTable('items', table => {
+        table.increments('id').primary;
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
+}
 
 /**
  * Go to back the table (Rollback)
  */
-export async function down() { }
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('items');
+}
--- backend/src/database/migrations/02_create_point_items.ts
+++ backend/src/database/migrations/02_create_point_items.ts
@@ -1,9 +1,25 @@
+import Knex from 'knex';
+
 /**
  * Create the table
  */
-export async function up() { }
+export async function up(knex: Knex) {
+    return knex.schema.createTable('point_items', table => {
+        table.increments('id').primary;
+        table.integer('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('ponts');
+        table.integer('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('items');
+    });
+}
 
 /**
  * Go to back the table (Rollback)
  */
-export async function down() { }
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_items');
+}
