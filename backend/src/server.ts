620fa73eb7c5c879c3de8618ec0fe2feb2b22ef3
ajfiumanee <ajfiumanee@outlook.com>
2020-06-14 14:18:10 -0300

    Creating seed file, uploaded SVG and created route get Items

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 app.use(express.json()); // Express understand requests JSON
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // Allow access to file urls
 
 app.listen(3333);
