4cf6b2f819d857ce67bced66b4637342999f5220
ajfiumanee <ajfiumanee@outlook.com>
2020-06-14 10:43:05 -0300

    Configuring Knex, creating table Points and First migration

---
+++ backend/src/database/migrations/00_create_points.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+
+/**
+ * Create the table
+ */
+export async function up(knex: Knex) {
+    return knex.schema.createTable('points', table => {
+        table.increments('id').primary;
+        table.string('image').notNullable();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.decimal('latitude').notNullable();
+        table.decimal('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+}
+
+/**
+ * Go to back the table (Rollback)
+ */
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('points');
+}
---
+++ backend/src/database/migrations/01_create_items.ts
@@ -0,0 +1,9 @@
+/**
+ * Create the table
+ */
+export async function up() { }
+
+/**
+ * Go to back the table (Rollback)
+ */
+export async function down() { }
